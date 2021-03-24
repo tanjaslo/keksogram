de175c80d8b15bca3dc1115590ae04761c0d5da1
tanjaslo <tanja.slo@ukr.net>
2021-03-24 22:08:49 +0100

    finishing slider

--- js/effect.js
+++ js/effect.js
@@ -1,96 +1,24 @@
 /* global noUiSlider:readonly */
 import {uploadPreviewElement, pictureEditForm, effectLevelContainer} from './upload.js';
-
-const effects = {
-  chrome: {
-    range: {
-      min: 0,
-      max: 1,
-    },
-    start: 1,
-    step: 0.1,
-  },
-  sepia: {
-    range: {
-      min: 0,
-      max: 1,
-    },
-    start: 1,
-    step: 0.1,
-  },
-  marvin: {
-    range: {
-      min: 0,
-      max: 100,
-    },
-    start: 100,
-    step: 1,
-  },
-  phobos: {
-    range: {
-      min: 0,
-      max: 3,
-    },
-    start: 3,
-    step: 0.1,
-  },
-  heat: {
-    range: {
-      min: 1,
-      max: 3,
-    },
-    start: 3,
-    step: 0.1,
-  },
-};
+import {effects} from './util.js';
 
 const effectsListForm = pictureEditForm.querySelector('.effects__list');
 const sliderElement = pictureEditForm.querySelector('.effect-level__slider');
 const effectLevelValue = pictureEditForm.querySelector('.effect-level__value');
 
 window.noUiSlider.create(sliderElement, {
-    range: {
-        min: 0,
-        max: 100,
-    },
-    start: 100,
-    step: 1,
-    connect: 'lower',
-    format: {
-      to: function (value) {
-        return value;
-      },
-      from: function (value) {
-        return parseFloat(value);
-      },
-    },
-  });
-
-const onEffectsListChange = (effect) => {
-  const addEffectToPicture = (evt) => {
-    const currentEffect = evt.target.value;
-
-    uploadPreviewElement.classList.toggle(`effects__preview--${currentEffect}`);
-    sliderElement.noUiSlider.set(100);
-
-    if (currentEffect === 'none') {
-      //Для эффекта «Оригинал» CSS-стили filter удаляются.
-      // При выборе эффекта «Оригинал» слайдер скрывается.
-      //effectLevelContainer.classList.add('hidden');
-      effectLevelContainer.style.display = 'none';
-      uploadPreviewElement.style.filter = 'none';
-    } else {
-      //effectLevelContainer.classList.remove('hidden');
-      effectLevelContainer.style.display = 'block';
-      sliderElement.noUiSlider.updateOptions(effects[currentEffect]);
-    }
-  };
-  uploadPreviewElement.className = '';
-  addEffectToPicture(effect);
-};
+  range: {
+      min: 0,
+      max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
 
 sliderElement.noUiSlider.on('update', (_, handle, unencoded) => {
   const effect = document.querySelector('.effects__radio:checked').value;
+  //  Кроме визуального применения эффекта необходимо записывать значение в скрытое поле для дальнейшей отправки на сервер.
   const value = unencoded[handle];
   effectLevelValue.value = value;
 
@@ -113,20 +41,31 @@ switch (effect) {
   }
 });
 
-effectsListForm.addEventListener('click', onEffectsListChange);
-// удалить при закрытии
+const onEffectsListChange = (effect) => {
 
-//  Кроме визуального применения эффекта необходимо записывать значение в скрытое поле для дальнейшей отправки на сервер. Уровень эффекта записывается в поле  effectLevelValue
+  const addEffectToPicture = (evt) => {
+    if (!(evt.target instanceof HTMLInputElement)) {
+      return;
+    }
+    const currentEffect = evt.target.value;
+    sliderElement.noUiSlider.set(100);
+    uploadPreviewElement.classList.toggle(`effects__preview--${currentEffect}`);
+
+    if (currentEffect === 'none') {
+      effectLevelContainer.style.display = 'none';
+      uploadPreviewElement.style.filter = 'none';
+    } else {
+      effectLevelContainer.style.display = 'block';
+      sliderElement.noUiSlider.updateOptions(effects[currentEffect]);
+    }
+  };
+  uploadPreviewElement.className = '';
+  addEffectToPicture(effect);
+};
+
+effectsListForm.addEventListener('click', onEffectsListChange);
 
 /*
-
-effectsListForm.addEventListener('change', (evt) => {
-  const effect = evt.target.value;
-  console.log(effect);
-  sliderElement.noUiSlider.updateOptions(effects[effect]);
-});
-
-
 При изменении уровня интенсивности эффекта, CSS-стили картинки uploadPreviewElement
 обновляются следующим образом:
 uploadPreviewElement.style.filter = `${currentEffect}(${value / 100})`;
@@ -141,20 +80,14 @@ uploadPreviewElement.style.filter = `${currentEffect}(${value / 100})`;
 При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%): слайдер, CSS-стиль изображения и значение поля должны обновляться.
 
 Обратите внимание, что при переключении фильтра, уровень эффекта должен сразу сбрасываться до начального состояния, т. е. логика по определению уровня насыщенности должна срабатывать не только при «перемещении» слайдера, но и при переключении фильтров.
+
+
+effectsListForm.addEventListener('change', (evt) => {
+  const effect = evt.target.value;
+  console.log(effect);
+  sliderElement.noUiSlider.updateOptions(effects[effect]);
+});
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* const addEffectsToPicture = () => {
   const currentEffect = document.querySelector('.effects__radio:checked').value;
--- js/util.js
+++ js/util.js
@@ -1,6 +1,49 @@
 const ALERT_MESSAGE = 'Не удалось загрузить данные';
 const ALERT_SHOW_TIME = 5000;
 
+const effects = {
+  chrome: {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  sepia: {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  marvin: {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+  },
+  phobos: {
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  heat: {
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+};
+
 const showAlert = () => {
   const alertContainer = document.createElement('div');
   alertContainer.classList.add('alert-container');
@@ -35,4 +78,4 @@ const closeModal = (element) => {
   element.classList.add('hidden');
 };
 
-export {showAlert, isEscEvent, openModal, closeModal}
+export {showAlert, isEscEvent, openModal, closeModal, effects}
