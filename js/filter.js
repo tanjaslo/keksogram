482372713243fe5afa6f9f6d9e0de7094e9acd5b
tanjaslo <tanja.slo@ukr.net>
2021-03-29 14:31:49 +0200

    filters added

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,104 @@
-const filterDefault = document.querySelector('#filter-default');
-const filterDefault = document.querySelector('#filter-random');
-const filterDefault = document.querySelector('#filter-discussed');
+import { removeMarkers, setMarkers } from './map.js';
+import { debounce } from './util.js';
 
-/* Добавьте обработчики изменения фильтров, которые будут управлять порядком отрисовки элементов на странице:
-По умолчанию — фотографии в изначальном порядке с сервера.
-Случайные — 10 случайных, не повторяющихся фотографий.
-Обсуждаемые — фотографии, отсортированные в порядке убывания количества комментариев. */
+const mapFiltersForm = document.querySelector('.map__filters');
+const mapFilters = mapFiltersForm.querySelectorAll('.map__filter')
+const mapFeatures = document.querySelector('.map__features');
+const housingTypeFilter = mapFiltersForm.querySelector('#housing-type');
+const housingPriceFilter = mapFiltersForm.querySelector('#housing-price');
+const housingRoomsFilter = mapFiltersForm.querySelector('#housing-rooms');
+const housingGuestsFilter = mapFiltersForm.querySelector('#housing-guests');
 
+const MAX_FILTERED_COUNT = 10;
+const RERENDER_DELAY = 500;
+const PRICE_LOW = 10000;
+const PRICE_HIGH = 50000;
+
+const deactivateFilterForm = () => {
+  mapFiltersForm.classList.add('map__filters--disabled');
+  mapFilters.forEach((filter) => {
+    filter.setAttribute('disabled', '');
+  });
+  mapFeatures.setAttribute('disabled', 'disabled');
+};
+
+const activateFilterForm = () => {
+  mapFiltersForm.classList.remove('map__filters--disabled');
+  mapFilters.forEach((filter) => {
+    filter.removeAttribute('disabled');
+  });
+  mapFeatures.removeAttribute('disabled');
+};
+
+const featuresFilter = (advert) => {
+  const checkedFeatures = mapFiltersForm.querySelectorAll('.map__checkbox:checked');
+  let i = 0;
+  checkedFeatures.forEach((feature) => {
+    if (advert.offer.features.includes(feature.value)) {
+      i++;
+    }
+  });
+  return i === checkedFeatures.length;
+};
+
+const typeFilter  = (advert) => {
+  return housingTypeFilter.value === 'any' || advert.offer.type === housingTypeFilter.value;
+};
+
+const roomsFilter = (advert) => {
+  return housingRoomsFilter.value === 'any' || advert.offer.rooms === Number(housingRoomsFilter.value);
+};
+
+const capacityFilter = (advert) => {
+  return housingGuestsFilter.value === 'any' || advert.offer.guests === Number(housingGuestsFilter.value);
+};
+
+const priceFilter = (advert) => {
+  const filterPrice = housingPriceFilter.value;
+  const advertPrice = advert.offer.price;
+
+  switch (filterPrice) {
+    case 'middle': return advertPrice >= PRICE_LOW && advertPrice < PRICE_HIGH;
+    case 'low': return advertPrice < PRICE_LOW;
+    case 'high': return advertPrice >= PRICE_HIGH;
+    default: return true;
+  }
+};
+
+const isAdvertMatched = (advert) => {
+  return featuresFilter(advert) &&
+  roomsFilter(advert) &&
+  capacityFilter(advert) &&
+  priceFilter(advert) &&
+  typeFilter(advert);
+};
+
+const getFilteredAdverts = (adverts) => {
+  const filteredAdverts = [];
+  for (let i = 0; i < adverts.length; i++) {
+    const advert = adverts[i];
+    if (isAdvertMatched(advert)) {
+      filteredAdverts.push(advert);
+    }
+    if (filteredAdverts.length === MAX_FILTERED_COUNT) {
+      return filteredAdverts;
+    }
+  }
+  return filteredAdverts;
+};
+
+const debouncedOnFilterChange = debounce((adverts) => {
+  onFilterChange(adverts)
+}, RERENDER_DELAY);
+
+const onFilterChange = (adverts) => {
+  const filteredAdverts = getFilteredAdverts(adverts);
+  removeMarkers();
+  setMarkers(filteredAdverts);
+};
+
+const initFilterChangeListener = (adverts) => {
+  mapFiltersForm.addEventListener('change', () => debouncedOnFilterChange(adverts));
+};
+
+export { mapFiltersForm, deactivateFilterForm, activateFilterForm, initFilterChangeListener }
--- js/util.js
+++ js/util.js
@@ -1,93 +1,91 @@
-const ALERT_MESSAGE = 'Не удалось загрузить данные';
 const ALERT_SHOW_TIME = 5000;
+const ALERT_MESSAGE = 'Не удалось загрузить данные с сервера :(';
 
-const effects = {
-  chrome: {
-    range: {
-      min: 0,
-      max: 1,
-    },
-    start: 1,
-    step: 0.1,
-  },
-  sepia: {
-    range: {
-      min: 0,
-      max: 1,
-    },
-    start: 1,
-    step: 0.1,
-  },
-  marvin: {
-    range: {
-      min: 0,
-      max: 100,
-    },
-    start: 100,
-    step: 1,
-  },
-  phobos: {
-    range: {
-      min: 0,
-      max: 3,
-    },
-    start: 3,
-    step: 0.1,
-  },
-  heat: {
-    range: {
-      min: 1,
-      max: 3,
-    },
-    start: 3,
-    step: 0.1,
-  },
+const HOUSING = {
+  bungalow: 'Бунгало',
+  flat: 'Квартира',
+  house: 'Дом',
+  palace: 'Дворец',
 };
 
-// имя_функции(проверяемая_строка, максимальная длина);
-// Результат: true, если строка проходит по длине, и false — если не проходит
-// const MAX_LENGTH = 15;
-
-const isCharLimit = (string, length) => {
-  return (string.length > length) ? true : false;
-};
-// console.log(getStringLength('good morning', MAX_LENGTH));
-
-/*
-// Результат: целое число из диапазона "от...до"
-const getRandomIntegerInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (min >= max || min < 0 || max < 0) ? null : Math.floor(Math.random() * (max - min +1) + min)
+const HOUSING_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
 };
 
-const getRandomArrayElement = (elements) => {
-  const randomArrayIndex = getRandomIntInclusive(0, elements.length - 1);
-  return elements[randomArrayIndex];
-}; */
+const getHousingType = (type) => {
+  return HOUSING[type];
+};
 
-const showAlert = () => {
-  const alertContainer = document.createElement('div');
-  alertContainer.classList.add('alert-container');
-  alertContainer.textContent = ALERT_MESSAGE;
-  document.body.append(alertContainer);
-  setTimeout(() => {
-    alertContainer.remove();
-  }, ALERT_SHOW_TIME);
+const getHousingPrice = (type) => {
+  return HOUSING_PRICE[type];
+};
+
+const declOfRoomsNumber = (rooms) => {
+  const val = rooms % 10;
+  const val2 = rooms % 100;
+  if ([11, 12, 13, 14].includes(val2)) {
+    return `${rooms} комнат`;
+  }
+  if (val === 1) {
+    return `${rooms} комната`;
+  }
+  if ([2,3,4].includes(val)) {
+    return `${rooms} комнаты`;
+  }
+  return `${rooms} комнат`;
+};
+
+const declOfGuestsNumber = (guests) => {
+  return guests === 1 || guests > 20 && guests % 10 === 1 && guests % 100 !== 11 ? `${guests} гостя` : `${guests} гостей`;
 };
 
 const isEscEvent = (evt) => {
   return evt.key === 'Escape' || evt.key === 'Esc';
 };
 
-const openModal = (element) => {
-  document.body.classList.add('modal-open');
-  element.classList.remove('hidden');
+const showAlert = () => {
+  const alertContainer = document.createElement('div');
+
+  alertContainer.style.zIndex = 9999;
+  alertContainer.style.position = 'fixed';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '28px';
+  alertContainer.style.fontStyle = 'Tahoma';
+  alertContainer.style.color = '#ffffff';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = ALERT_MESSAGE;
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
 };
 
-const closeModal = (element) => {
-  document.body.classList.remove('modal-open');
-  element.classList.add('hidden');
+const debounce = (callback, time) => {
+  let interval;
+  return (...args) => {
+    clearTimeout(interval);
+    interval = setTimeout(() => {
+      interval = null;
+      callback(...args);
+    }, time);
+  };
 };
 
-export {isEscEvent, isCharLimit, showAlert, openModal, closeModal, effects}
+export {
+  declOfRoomsNumber,
+  declOfGuestsNumber,
+  getHousingType,
+  getHousingPrice,
+  showAlert,
+  isEscEvent,
+  debounce
+};
